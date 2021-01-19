d918533a54de5590778fe6b340d4e90cb926f95a
nycpivot <mijames@vmware.com>
2021-01-19 13:42:27 -0500

    Added dotnet-core app.

--- tanzu-azure-gbb/demo-live.sh
+++ tanzu-azure-gbb/demo-live.sh
@@ -7,7 +7,7 @@ kp image list
 
 
 #TANZU BUILD SERVICE
-kp image create spring-music --tag tanzuregitry.azurecr.io/spring-music --git https://github.com/cloudfoundry-samples/spring-music.git
+kp image create spring-music --tag tanzuregistry.azurecr.io/spring-music --git https://github.com/cloudfoundry-samples/spring-music.git
 
 kp image status spring-music
 #kp build logs spring-music
@@ -20,3 +20,12 @@ cat deployment.yaml
 kubectl apply -f deployment.yaml
 
 #kubectl set image deployment tbs-deployment tbs-core-web-mvc=${acrserver}/tbs-core-web-mvc:latest
+
+
+
+
+
+kp image create aspnet-core --tag tanzuregistry.azurecr.io/aspnet-core --git https://github.com/nycpivot/tanzu-build-service.git
+
+
+kp image create dotnet-core --tag tanzuregistry.azurecr.io/dotnet-core --git https://github.com/nycpivot/dotnet-docker.git
