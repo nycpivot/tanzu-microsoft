478d0d6e711237e244a1b8e454ad483dfb18804b
nycpivot <mijames@vmware.com>
2021-01-22 18:20:22 -0500

    Ongoing changes.

--- tanzu-azure-gbb/00-prep.sh
+++ tanzu-azure-gbb/00-prep.sh
@@ -38,7 +38,7 @@ gcloud container clusters delete "tanzu-azure-gke-aspnet-core" --region "us-east
 gcloud container clusters create "tanzu-azure-gke-aspnet-core" --region "us-east1" --no-enable-basic-auth --cluster-version "1.16.15-gke.6000" --release-channel "None" --machine-type "e2-standard-4" --image-type "COS" --disk-type "pd-standard" --disk-size "100" --metadata disable-legacy-endpoints=true --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" --num-nodes "3" --enable-stackdriver-kubernetes --enable-ip-alias --network "projects/pa-mjames/global/networks/default" --subnetwork "projects/pa-mjames/regions/us-east1/subnetworks/default" --default-max-pods-per-node "110" --no-enable-master-authorized-networks --addons HorizontalPodAutoscaling,HttpLoadBalancing --enable-autoupgrade --enable-autorepair --max-surge-upgrade 1 --max-unavailable-upgrade 0
 gcloud container clusters get-credentials "tanzu-azure-gke-aspnet-core" --region "us-east1"
 
-kubectl config use-context tanzu-azure-gke_pa-mjames_us-east1_tanzu-azure-gke-aspnet-core
+kubectl config use-context gke_pa-mjames_us-east1_tanzu-azure-gke-aspnet-core
 
 kubectl create secret docker-registry tanzuregistry-secret \
 	--docker-server=${acr_server} \
