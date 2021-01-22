af4b4d4179bd8cff48d73dd267f4fe70498f2d0f
nycpivot <mijames@vmware.com>
2021-01-21 19:50:04 -0500

    Created policy files.

--- tanzu-azure-gbb/00-prep.sh
+++ tanzu-azure-gbb/00-prep.sh
@@ -28,12 +28,12 @@ tkg get credentials tanzu-azure-tkg-aspnet-core-admin@tanzu-azure-tkg-aspnet-cor
 #CREATE GKE CLUSTER
 gcloud auth login
 
-read -p "Logging into GCP..." var1
+read -p "WAITING FOR TOKEN..." var1
 
 gcloud config set project pa-mjames
 gcloud container clusters delete "tanzu-azure-gke-aspnet-core" --region "us-east1"
 gcloud container clusters create "tanzu-azure-gke-aspnet-core" --region "us-east1" --no-enable-basic-auth --cluster-version "1.16.15-gke.6000" --release-channel "None" --machine-type "e2-standard-4" --image-type "COS" --disk-type "pd-standard" --disk-size "100" --metadata disable-legacy-endpoints=true --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" --num-nodes "3" --enable-stackdriver-kubernetes --enable-ip-alias --network "projects/pa-mjames/global/networks/default" --subnetwork "projects/pa-mjames/regions/us-east1/subnetworks/default" --default-max-pods-per-node "110" --no-enable-master-authorized-networks --addons HorizontalPodAutoscaling,HttpLoadBalancing --enable-autoupgrade --enable-autorepair --max-surge-upgrade 1 --max-unavailable-upgrade 0
-
+gcloud container clusters get-credentials "tanzu-azure-gke-aspnet-core" --region "us-east1"
 
 #DELETE KP IMAGES
 kubectl config use-context tanzu-build-service
--- tanzu-azure-gbb/01-live.sh
+++ tanzu-azure-gbb/01-live.sh
@@ -26,25 +26,71 @@ clear
 
 
 #TBS
+DEMO_PROMPT="${GREEN} TBS ${BLUE}\W "
+
 pe "kubectl config use-context tanzu-build-service"
 
 pe "kp image list"
 
 #CREATE IMAGES NOW - DURING EXECUTION EXPLAIN COMMAND PARAMS, LIST ALL TARGET CLUSTERS, CHECK STATUS or CHECK BUILD LOGS
-pe "kp image create spring-music --tag tanzuregistry.azurecr.io/spring-music --git https://github.com/cloudfoundry-samples/spring-music.git"
-pe "kp image create aspnet-core --tag tanzuregistry.azurecr.io/aspnet-core --git https://github.com/nycpivot/dotnet-docker.git"
+#pe "kp image create spring-music --tag tanzuregistry.azurecr.io/spring-music --git https://github.com/cloudfoundry-samples/spring-music.git"
+#pe "kp image create aspnet-core --tag tanzuregistry.azurecr.io/aspnet-core --git https://github.com/nycpivot/dotnet-docker.git"
+
+pe "kp image create aspnet-core-live --tag tanzuregistry.azurecr.io/aspnet-core-live --git https://github.com/nycpivot/dotnet-docker.git"
 
 pe "kubectl config get-contexts --output name"
 
 #CHECK STATUS OF IMAGES
-pe "kp image status spring-music"
-pe "kp build logs spring-music"
+#pe "kp image status spring-music"
+#pe "kp build logs spring-music"
 
-pe "kp image status aspnet-core"
+#pe "kp image status aspnet-core"
 #kp build logs aspnet-core
 
+pe "kp image status aspnet-core-live"
 
-#
+
+#TMC PORTAL - ATTACH AKS CLUSTER
+DEMO_PROMPT="${GREEN} TMC-AKS ${CYAN}\W "
+
+pe "kubectl config use-context tanzu-azure-aks-spring-music"
+pe "kubectl get ns"
+
+#pe "tmc cluster attach --name tanzu-azure-aks-spring-music --cluster-group development"
+
+pe "kubectl get ns"
 
 
-#TMC PORTAL - ATTACH TKG CLUSTER
+#TKG - DEPLOY APPS
+DEMO_PROMPT="${GREEN} TKG-GKE-TKG ${YELLOW}\W "
+
+pe "kubectl config use-context gke_pa-mjames_us-east1_tanzu-azure-gke-aspnet-core"
+
+pe "cat deployment-aspnet-core.yaml"
+pe "kubectl apply -f deployment-aspnet-core.yaml"
+pe "kubectl get svc tanzu-azure-gke-aspnet-core"
+
+pe "kubectl config use-context tanzu-azure-tkg-aspnet-core"
+
+pe "cat deployment-aspnet-core.yaml"
+pe "kubectl apply -f deployment-aspnet-core.yaml"
+pe "kubectl get svc tanzu-azure-gke-aspnet-core"
+
+
+#TMC - CREATE POLICIES
+#tmc clustergroup namespace-quota-policy create -r custom -i --cluster-group-name development --dry-run
+pe "tmc clustergroup namespace-quota-policy create -f quota-development-policy.yaml"
+
+#tmc clustergroup security-policy create -r custom -i --cluster-group-name production --dry-run
+pe "tmc clustergroup security-policy create -f security-production-policy.yaml"
+
+
+pe "kubectl run nginx --image nginx"
+pe "kubectl delete pod nginx"
+#tmc organization image-policy create -r custom -i --dry-run
+pe "tmc organization image-policy create -f registry-nycpivot-policy.yaml"
+pe "kubectl run nginx --image nginx"
+
+
+
+
