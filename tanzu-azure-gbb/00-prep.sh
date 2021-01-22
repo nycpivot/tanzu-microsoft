78d854eded3319ed69c25d578605a57d5aca685b
nycpivot <mijames@vmware.com>
2021-01-22 10:40:34 -0500

    Ongoing changes.

--- tanzu-azure-gbb/00-prep.sh
+++ tanzu-azure-gbb/00-prep.sh
@@ -15,6 +15,15 @@ az aks create --name tanzu-azure-aks-spring-music --resource-group tanzu-azure -
 az aks get-credentials --name tanzu-azure-aks-spring-music --resource-group tanzu-azure
 
 
+#CREATE GKE CLUSTER
+gcloud auth login
+
+gcloud config set project pa-mjames
+gcloud container clusters delete "tanzu-azure-gke-aspnet-core" --region "us-east1"
+gcloud container clusters create "tanzu-azure-gke-aspnet-core" --region "us-east1" --no-enable-basic-auth --cluster-version "1.16.15-gke.6000" --release-channel "None" --machine-type "e2-standard-4" --image-type "COS" --disk-type "pd-standard" --disk-size "100" --metadata disable-legacy-endpoints=true --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" --num-nodes "3" --enable-stackdriver-kubernetes --enable-ip-alias --network "projects/pa-mjames/global/networks/default" --subnetwork "projects/pa-mjames/regions/us-east1/subnetworks/default" --default-max-pods-per-node "110" --no-enable-master-authorized-networks --addons HorizontalPodAutoscaling,HttpLoadBalancing --enable-autoupgrade --enable-autorepair --max-surge-upgrade 1 --max-unavailable-upgrade 0
+gcloud container clusters get-credentials "tanzu-azure-gke-aspnet-core" --region "us-east1"
+
+
 #CREATE TKG CLUSTER
 sudo tkg delete cluster tanzu-azure-tkg-aspnet-core
 sleep 15m
@@ -25,14 +34,6 @@ read -p "PRESS ANY KEY TO CONTINUE" ok
 sudo tkg create cluster tanzu-azure-tkg-aspnet-core --plan prod
 tkg get credentials tanzu-azure-tkg-aspnet-core-admin@tanzu-azure-tkg-aspnet-core
 
-
-#CREATE GKE CLUSTER
-gcloud auth login
-
-gcloud config set project pa-mjames
-gcloud container clusters delete "tanzu-azure-gke-aspnet-core" --region "us-east1"
-gcloud container clusters create "tanzu-azure-gke-aspnet-core" --region "us-east1" --no-enable-basic-auth --cluster-version "1.16.15-gke.6000" --release-channel "None" --machine-type "e2-standard-4" --image-type "COS" --disk-type "pd-standard" --disk-size "100" --metadata disable-legacy-endpoints=true --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" --num-nodes "3" --enable-stackdriver-kubernetes --enable-ip-alias --network "projects/pa-mjames/global/networks/default" --subnetwork "projects/pa-mjames/regions/us-east1/subnetworks/default" --default-max-pods-per-node "110" --no-enable-master-authorized-networks --addons HorizontalPodAutoscaling,HttpLoadBalancing --enable-autoupgrade --enable-autorepair --max-surge-upgrade 1 --max-unavailable-upgrade 0
-gcloud container clusters get-credentials "tanzu-azure-gke-aspnet-core" --region "us-east1"
 
 #DELETE KP IMAGES
 kubectl config use-context tanzu-build-service
@@ -50,12 +51,12 @@ tmc organization image-policy delete registry-nycpivot-policy
 read -p "tmc tanzu-azure-tkg-aspnet-core attach url: " tkg_attach_url
 
 kubectl config use-context tanzu-azure-tkg-aspnet-core-admin@tanzu-azure-tkg-aspnet-core
-#$tkg_attach_url
+$tkg_attach_url
 
 read -p "tmc tanzu-azure-gke-aspnet-core attach url: " gke_attach_url
 
 kubectl config use-context gke_pa-mjames_us-east1_tanzu-azure-gke-aspnet-core
-#$gke_attach_url
+$gke_attach_url
 
 
 #DOWNLOAD FILES
