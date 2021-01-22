dcf5dae40049c187b86bbb4bf22176f9919ed264
nycpivot <mijames@vmware.com>
2021-01-21 22:34:31 -0500

    Finished 00-prep.

--- tanzu-azure-gbb/00-prep.sh
+++ tanzu-azure-gbb/00-prep.sh
@@ -33,8 +33,6 @@ tkg get credentials tanzu-azure-tkg-aspnet-core-admin@tanzu-azure-tkg-aspnet-cor
 #CREATE GKE CLUSTER
 gcloud auth login
 
-read -p "WAITING FOR TOKEN..." var1
-
 gcloud config set project pa-mjames
 gcloud container clusters delete "tanzu-azure-gke-aspnet-core" --region "us-east1"
 gcloud container clusters create "tanzu-azure-gke-aspnet-core" --region "us-east1" --no-enable-basic-auth --cluster-version "1.16.15-gke.6000" --release-channel "None" --machine-type "e2-standard-4" --image-type "COS" --disk-type "pd-standard" --disk-size "100" --metadata disable-legacy-endpoints=true --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" --num-nodes "3" --enable-stackdriver-kubernetes --enable-ip-alias --network "projects/pa-mjames/global/networks/default" --subnetwork "projects/pa-mjames/regions/us-east1/subnetworks/default" --default-max-pods-per-node "110" --no-enable-master-authorized-networks --addons HorizontalPodAutoscaling,HttpLoadBalancing --enable-autoupgrade --enable-autorepair --max-surge-upgrade 1 --max-unavailable-upgrade 0
@@ -55,15 +53,13 @@ tmc organization image-policy delete registry-nycpivot-policy
 
 read -p "tmc tanzu-azure-tkg-spring-music attach url: " tkg_attach_url
 
-kubectl config use-context tanzu-azure-tkg-spring-music
-kubectl create -f $tkg_attach_url
+kubectl config use-context tanzu-azure-tkg-aspnet-core-admin@tanzu-azure-tkg-aspnet-core
+#$tkg_attach_url
 
 read -p "tmc tanzu-azure-gke-aspnet-core attach url: " gke_attach_url
 
 kubectl config use-context gke_pa-mjames_us-east1_tanzu-azure-gke-aspnet-core
-kubectl create -f $gke_attach_url
-
-
+#$gke_attach_url
 
 
 #DOWNLOAD FILES
