3701fa795f1168bca19ee78b53afd1ee5415cf6b
nycpivot <mijames@vmware.com>
2021-01-22 20:07:51 -0500

    Ongoing changes.

--- tanzu-azure-gbb/02-tmc.sh
+++ tanzu-azure-gbb/02-tmc.sh
@@ -1,3 +1,29 @@
+#!/bin/bash
+
+########################
+# include the magic
+########################
+. demo-magic.sh
+
+########################
+# Configure the options
+########################
+
+#
+# speed at which to simulate typing. bigger num = faster
+#
+TYPE_SPEED=10
+
+#
+# custom prompt
+#
+# see http://www.tldp.org/HOWTO/Bash-Prompt-HOWTO/bash-prompt-escape-sequences.html for escape sequences
+#
+#DEMO_PROMPT="${GREEN}➜ ${CYAN}\W "
+
+# hide the evidence
+clear
+
 #TMC PORTAL - ATTACH AKS CLUSTER
 DEMO_PROMPT="${GREEN}➜ TMC-AKS ${CYAN}\W "
 
