57d0398273f3323b10b9488800093b1fedb6ca78
nycpivot <mijames@vmware.com>
2021-01-22 21:38:29 -0500

    Ongoing changes.\

--- tanzu-azure-gbb/04-policy.sh
+++ tanzu-azure-gbb/04-policy.sh
@@ -27,16 +27,24 @@ clear
 DEMO_PROMPT="${GREEN}➜ TMC POLICY ${YELLOW}\W "
 
 
-#TMC - CREATE POLICIES
-#tmc clustergroup namespace-quota-policy create -r custom -i --cluster-group-name development --dry-run
-pe "tmc clustergroup namespace-quota-policy create -f quota-development-policy.yaml"
+#TMC POLICY
 
-#tmc clustergroup security-policy create -r custom -i --cluster-group-name production --dry-run
-pe "tmc clustergroup security-policy create -f security-production-policy.yaml"
-
-
+#-image registry
 pe "kubectl run nginx --image nginx"
+pe "kubectl get pods"
 pe "kubectl delete pod nginx"
 #tmc organization image-policy create -r custom -i --dry-run
 pe "tmc organization image-policy create -f registry-nycpivot-policy.yaml"
 pe "kubectl run nginx --image nginx"
+
+
+#-quota policy
+#tmc clustergroup namespace-quota-policy create -r custom -i --cluster-group-name development --dry-run
+pe "tmc clustergroup namespace-quota-policy create -f quota-development-policy.yaml"
+
+
+#-network policy
+#tmc clustergroup security-policy create -r custom -i --cluster-group-name production --dry-run
+pe "tmc clustergroup security-policy create -f security-production-policy.yaml"
+
+
