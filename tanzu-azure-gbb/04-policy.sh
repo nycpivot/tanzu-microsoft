2bf6a6eadd6dfe30ca17120866c79b5a9a56bbc1
nycpivot <mijames@vmware.com>
2021-01-22 21:49:42 -0500

    Ongoing changes.

--- tanzu-azure-gbb/04-policy.sh
+++ tanzu-azure-gbb/04-policy.sh
@@ -43,6 +43,16 @@ pe "kubectl run nginx --image nginx"
 #tmc clustergroup namespace-quota-policy create -r custom -i --cluster-group-name development --dry-run
 pe "tmc clustergroup namespace-quota-policy create -f quota-development-policy.yaml"
 
+pe "kubectl config use-context tanzu-azure-aks-spring-music"
+pe "cat deployment-spring-music-quota.yaml"
+pe "kubectl apply -f deployment-spring-music-quota.yaml"
+
+pe "kubectl config use-context gke_pa-mjames_us-east1_tanzu-azure-gke-aspnet-core"
+pe "kubectl apply -f deployment-spring-music-quota.yaml.yaml"
+
+pe "kubectl config use-context tanzu-azure-tkg-aspnet-core-admin@tanzu-azure-tkg-aspnet-core"
+pe "kubectl apply -f deployment-spring-music-quota.yaml.yaml"
+
 
 #-network policy
 #tmc clustergroup security-policy create -r custom -i --cluster-group-name production --dry-run
